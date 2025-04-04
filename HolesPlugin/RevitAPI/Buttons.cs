b054e9228c343af9d5eb066367b1776cc2ac29a7
Степан Юзефович <yuzefovich1stepan@gmail.com>
2025-04-04 10:49:41 +0300

    Подготовка программы для Revit Api, написание базовых интерфейсов, установка логотипа

---
+++ HolesPlugin/RevitAPI/Buttons.cs
@@ -0,0 +1,69 @@
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System;
+using System.Reflection;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Autodesk.Revit.Exceptions;
+
+namespace HolesPlugin
+{
+    internal class Buttons : IExternalApplication
+    {
+        string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+        string tabName = "ETM";
+
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RibbonPanel panel;
+            try
+            {
+                panel = application.GetRibbonPanels(tabName).First();
+            }
+            catch (Exception)
+            {
+                application.CreateRibbonTab(tabName);
+            }
+
+            panel = application.CreateRibbonPanel(tabName, "Отверстия");
+
+            panel.AddItem(new PushButtonData(nameof(MainRevitFunction), "Отметка низа\nотносительно 0.000", assemblyLocation, typeof(MainRevitFunction).FullName)
+            {
+                LargeImage = GetBitmapImage(Properties.Resources.ETMHolesPluginLogo, 32, 32),
+                LongDescription = "Плагин для расположения отверстии относительно отметки 0.000"
+            });
+
+            return Result.Succeeded;
+        }
+
+        BitmapImage GetBitmapImage(Bitmap bitmap, int width, int height)
+        {
+            // Создание нового битмапа с заданными размерами
+            var resizedBitmap = new Bitmap(bitmap, new Size(width, height));
+
+            using (var memory = new MemoryStream())
+            {
+                resizedBitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+    }
+}
---
+++ HolesPlugin/RevitAPI/MainRevitFunction.cs
@@ -0,0 +1,34 @@
+﻿/*==========================================================
+                  _______ _______ __  __                
+                 |  _____|__   __|  \/  |           
+                 | |___     | |  | \  / | 
+                 |  ___|    | |  | |\/| |
+                 | |_____   | |  | |  | |
+                 |_______|  |_|  |_|  |_|
+
+[*] Developer:
+[*] Stepan Yuzefovich
+
+===========================================================*/
+
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolesPlugin
+{
+    [Transaction(TransactionMode.Manual)]
+    public class MainRevitFunction : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            
+        }
+    }
+}
